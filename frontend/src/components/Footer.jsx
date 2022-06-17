3d6a96b6f064f5b4d37d2041bc9fc551983db842
faizalajiw <faizalajiwibowo7@gmail.com>
2022-06-17 14:32:40 +0700

    first commit Navbar & Footer

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="bg-dark py-4 mt-4 text-center text-white">
+      &copy; Ijal 2022. All rights reserved.
+    </div>
+  );
+};
+
+export default Footer;

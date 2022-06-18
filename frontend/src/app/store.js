9c25c36762f2ad22ff164ec681e46a5a1121646e
faizalajiw <faizalajiwibowo7@gmail.com>
2022-06-18 10:13:40 +0700

    add redux library for store data

---
+++ frontend/src/app/store.js
@@ -0,0 +1,29 @@
+import { configureStore } from "@reduxjs/toolkit";
+import userSlice from "../features/slice/userSlice";
+import postSlice from "../features/slice/postSlice";
+import apiSlice from "../features/api/apiSlice";
+import storage from "redux-persist/lib/storage";
+import { combineReducers } from "redux";
+import { persistReducer } from "redux-persist";
+import thunk from "redux-thunk";
+
+const reducers = combineReducers({
+  user: userSlice,
+  post: postSlice,
+  [apiSlice.reducerPath]: apiSlice.reducer,
+});
+
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: [apiSlice.reducerPath],
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: [thunk, apiSlice.middleware],
+});
+
+export default store;

a5e43f6a853a4801b6405504d161beff5fe123d7
faizalajiw <faizalajiwibowo7@gmail.com>
2022-06-18 16:34:21 +0700

    update app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,45 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
 import Navbars from "./components/Navbars";
-import Login from "./pages/Login";
-import SignUp from "./pages/SignUp";
 import Footer from "./components/Footer";
+import SignUp from "./pages/SignUp";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import NewArticle from "./pages/NewArticle";
+import EditArticle from "./pages/EditArticle";
+import MyArticle from "./pages/MyArticle";
+import SingleArticle from "./pages/SingleArticle";
+import NotFound from "./pages/NotFound";
+import { useSelector } from "react-redux";
 
 const App = () => {
+  const { user } = useSelector((state) => state.user);
   return (
     <BrowserRouter>
       <Navbars />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" />
-        <Route path="/login" element ={<Login />} />
-        <Route path="/signup" element ={<SignUp />} />
+        {!user && (
+          <>
+            <Route path="/about" />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<SignUp />} />
+          </>
+        )}
+
+        {user && (
+          <>
+            <Route path="/new-article" element={<NewArticle />} />
+            <Route path="/articles/:id/edit" element={<EditArticle />} />
+            <Route path="/articles/me" element={<MyArticle />} />
+          </>
+        )}
+
+        <Route path="/articles/:id" element={<SingleArticle />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
       <Footer />
     </BrowserRouter>
   );
-}
+};
 
 export default App;

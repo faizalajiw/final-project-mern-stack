eaa529e124a8ef10d2569c0f6690e037ec4d0036
faizalajiw <faizalajiwibowo7@gmail.com>
2022-06-19 15:09:08 +0700

    fix render with remove StrictMode

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -10,11 +10,9 @@ import { persistStore } from "redux-persist";
 const persistor = persistStore(store);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <App />
       </PersistGate>
     </Provider>
-  </React.StrictMode>
 );

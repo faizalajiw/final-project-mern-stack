c0d9fd10b599f88be8182eb9532ff3454f2a4a4a
faizalajiw <faizalajiwibowo7@gmail.com>
2022-06-19 19:16:07 +0700

    @ Bhinneka Post 2022

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <div className="bg-dark py-4 mt-4 text-center text-white">
-      &copy; Ijal 2022. All rights reserved.
+      &copy; Bhinneka Post 2022. All rights reserved.
     </div>
   );
 };

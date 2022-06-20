da7dced9d6183c95c6cbe434ba14a48a0263ea25
faizalajiw <faizalajiwibowo7@gmail.com>
2022-06-20 16:49:25 +0700

    add extraReducers to get all post

--- frontend/src/features/slice/postSlice.js
+++ frontend/src/features/slice/postSlice.js
@@ -1,10 +1,16 @@
 import { createSlice } from '@reduxjs/toolkit'
+import apiSlice from '../api/apiSlice';
 
 const initialState = [];
 
 export const postSlice = createSlice({
   name: 'post',
   initialState,
+  extraReducers: builder => {
+    builder.addMatcher(apiSlice.endpoints.getAllPosts.matchFulfilled, (state, {payload}) => {
+       return payload;
+    })
+  }
 });
 
 export default postSlice.reducer
